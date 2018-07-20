f5762f0e07b78d2ba64236490d9bc263511be1cd
Dani Larsen <danilarsen16@yahoo.com>
2018-07-20 14:10:38 -0500

    readme edit

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -10,6 +10,7 @@ CREATE TABLE products(
 	department_name VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
 	stock_quantity INTEGER NOT NULL,
+	product_sales INTEGER NOT NULL,
 	UNIQUE KEY (item_id)
 );
 
@@ -18,38 +19,37 @@ CREATE TABLE departments(
 	department_id VARCHAR(250) NOT NULL,
 	department_name VARCHAR(100) NOT NULL,
     over_head_costs INTEGER NOT NULL,
-    product_sales INTEGER NOT NULL,
 	UNIQUE KEY (item_id)
 );
 
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Leash","RECREATION",15.00,150);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Leash","RECREATION",15.00,150, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Treats Basket","HEALTH & WELLNESS",20.00,200);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Treats Basket","HEALTH & WELLNESS",20.00,200, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Bowls","MISC",8.00,100);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Bowls","MISC",8.00,100, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Rain Jacket","CLOTHING",30.00,50);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Rain Jacket","CLOTHING",30.00,50, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Booties","CLOTHING",20.00,50);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Booties","CLOTHING",20.00,50, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Chew Toys","RECREATION",6.00,50);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Chew Toys","RECREATION",6.00,50, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Chicken Toothpaste","HEALTH & WELLNESS",10.00,25);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Chicken Toothpaste","HEALTH & WELLNESS",10.00,25, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Food","HEALTH & WELLNESS",30.00,75);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Food","HEALTH & WELLNESS",30.00,75, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Brush","HEALTH & WELLNESS",12.00,50);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Brush","HEALTH & WELLNESS",12.00,50, 0);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Dog Sunglasses","CLOTHING",15.00,50);
+INSERT INTO products(product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Dog Sunglasses","CLOTHING",15.00,50, 0);
     
